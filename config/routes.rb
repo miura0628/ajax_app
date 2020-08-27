d647ff1fbdd77c943fb134ba4311c705bb0de1f1
miura0628 <yousanpu834@gmail.com>
2020-08-27 11:28:40 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  get 'posts/:id', to: 'posts#checked'
 end
